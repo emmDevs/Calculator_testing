e1c55107e40048505993c67e7adea8aa2101395f
Emma Hack <ehack78@gmail.com>
2020-12-22 16:10:45 +0000

    e2e testing number 2 all passed

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -10,11 +10,44 @@ describe('calculator', () => {
     cy.get('.display').should('contain', '2')
   });
 
+  
   it('number buttons should update the display of running total', () => {
     cy.get('#number2').click()
     cy.get('#number8').click()
     cy.get('#number4').click()
     cy.get('#number6').click();
     cy.get('.display').should('contain', '2846')
+  });
+
+  it('Arithmetical operations should update the display with the result of the operation', () => {
+    cy.get('#number2').click()
+    cy.get('#operator_add').click()
+    cy.get('#number4').click()
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '6')
+  });
+
+  it('Arithmetical operations should update the display with the result of the operation', () => {
+    cy.get('#number4').click()
+    cy.get('#operator_subtract').click()
+    cy.get('#number2').click()
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2')
+  });
+
+  it('Arithmetical operations should update the display with the result of the operation', () => {
+    cy.get('#number8').click()
+    cy.get('#operator_divide').click()
+    cy.get('#number2').click()
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '4')
+  });
+
+  it('Arithmetical operations should update the display with the result of the operation', () => {
+    cy.get('#number4').click()
+    cy.get('#operator_multiply').click()
+    cy.get('#number2').click()
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '8')
   });
 })
