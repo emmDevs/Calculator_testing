69f69488af29f73e4648fde03ea9d241f27643a1
Emma Hack <ehack78@gmail.com>
2020-12-22 13:46:54 +0000

    first 5 tests passing

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -8,5 +8,29 @@ describe('App.vue', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
-  })
+  });
+  it('should be able to add numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 1
+    wrapper.vm.add('4');
+    expect(wrapper.vm.runningTotal).to.equal(5)
+  });
+  it('should be able to subtract numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  });
+  it('should be able to multipy numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
+  });
+  it('should be able to divide numbers', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  });
 })
