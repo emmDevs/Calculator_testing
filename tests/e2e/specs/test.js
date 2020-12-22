4ddf4d741e9fe732df19d2db2e5b79bd37709060
Emma Hack <ehack78@gmail.com>
2020-12-22 14:56:25 +0000

    first e2e test passed

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -8,5 +8,13 @@ describe('calculator', () => {
   it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
-  })
+  });
+
+  it('number buttons should update the display of running total', () => {
+    cy.get('#number2').click()
+    cy.get('#number8').click()
+    cy.get('#number4').click()
+    cy.get('#number6').click();
+    cy.get('.display').should('contain', '2846')
+  });
 })
